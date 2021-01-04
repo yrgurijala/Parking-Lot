9eaea05301b8ea9ac3767cc95785353fa6c427ad
yrgurijala <gurijalayashwanth31@gmail.com>
2021-01-04 16:44:12 -0600

    Made the code more readable

--- src/com/myparking/Main.java
+++ src/com/myparking/Main.java
@@ -2,23 +2,60 @@ package com.myparking;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args) throws FileNotFoundException
-    {
-        System.out.println("Welcome to Yash Parking Lot");
-        System.out.println("We charge $2 for every hour" + "\n");
-
-        InputFile files = new InputFile();
+    {   
         File file1 = new File("input1.txt");
-        //File file2 = new File("input2.txt");
-        //File file3 = new File("input3.txt");
-        //File file4 = new File("input4.txt");
-
-        files.processInput(file1);
-        //files.checkInput(file2);
-        //files.checkInput(file3);
-        //files.processInput(file4);
+        File file2 = new File("input2.txt");
+        File file3 = new File("input3.txt");
+        File file4 = new File("input4.txt");
+        File file5 = new File("input5.txt");
+        
+        int parkingCapacity1 = 3;
+        int parkingCapacity2 = 10;
+        int parkingCapacity3 = 10;
+        int parkingCapacity4 = 100;
+        int parkingCapacity5 = 100;
+        
+        double parkingLotPrice1 = 100;
+        double parkingLotPrice2 = 5;
+        double parkingLotPrice3 = 5;
+        double parkingLotPrice4 = 2;
+        double parkingLotPrice5 = 2;
+        
+        ParkingLot yashParkingLot1 = new ParkingLot(parkingCapacity1, parkingLotPrice1);
+        ParkingLot yashParkingLot2 = new ParkingLot(parkingCapacity2, parkingLotPrice2);
+        ParkingLot yashParkingLot3 = new ParkingLot(parkingCapacity3, parkingLotPrice3);
+        ParkingLot yashParkingLot4 = new ParkingLot(parkingCapacity4, parkingLotPrice4);
+        ParkingLot yashParkingLot5 = new ParkingLot(parkingCapacity5, parkingLotPrice5);
+       
+        System.out.println("Yash Parking Lot: Area 1");
+        System.out.println("$" + String.format("%.2f", parkingLotPrice1) + " for every hour in this parking lot" + "\n");
+        yashParkingLot1.processInput(file1);
+        
+        System.out.println("Yash Parking Lot: Area 2");
+        System.out.println("$" + String.format("%.2f",parkingLotPrice2) + " for every hour in this parking lot" + "\n");
+        yashParkingLot2.processInput(file2);
+        
+        System.out.println("Yash Parking Lot: Area 3");
+        System.out.println("$" + String.format("%.2f",parkingLotPrice3) + " for every hour in this parking lot" + "\n");
+        yashParkingLot3.processInput(file3);
+        
+        System.out.println("Yash Parking Lot: Area 4");
+        System.out.println("$" + String.format("%.2f",parkingLotPrice4) + " for every hour in this parking lot" + "\n");
+        yashParkingLot4.processInput(file4);
+        
+        System.out.println("Yash Parking Lot: Area 5");
+        System.out.println("$" + String.format("%.2f",parkingLotPrice5) + " for every hour in this parking lot" + "\n");
+        yashParkingLot5.processInput(file5);
+        
+        
+        System.out.println("All revenue combined: $" + (yashParkingLot1.getTotalRevenue() + yashParkingLot2.getTotalRevenue() + 
+        												yashParkingLot3.getTotalRevenue() +yashParkingLot4.getTotalRevenue() + 
+        												yashParkingLot5.getTotalRevenue() ));
+        
     }
 }
--- src/com/myparking/ParkingLot.java
+++ src/com/myparking/ParkingLot.java
@@ -1,18 +1,25 @@
 package com.myparking;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
-public abstract class ParkingLot {
+public class ParkingLot {
 
     public int capacity;
     public LinkedList<Car> noOfCars = new LinkedList<Car>();
+    double totalRevenue = 0;
+    double pricePerHour = 0;
 
-    abstract void exit(String a, int b, int c, int d);
-    abstract int pricing(Car a);
-
-    ParkingLot(int a)
+    ParkingLot(int a, double b)
     {
         capacity = a;
+        pricePerHour = b;;
+    }
+    
+    public double getTotalRevenue() 
+    {
+    	return totalRevenue;
     }
 
     public boolean hasInstance(String a)
@@ -31,6 +38,51 @@ public abstract class ParkingLot {
 
         return false;
     }
+    
+    public void displayAllCars()
+    {
+        int check = 0;
+
+        while (check != noOfCars.size())
+        {
+            Car display = noOfCars.get(check);
+            System.out.println("	" + display.getName());
+            check++;
+        }
+    }
+
+    public void processInput(File a) throws FileNotFoundException
+    {
+        Scanner scanner = new Scanner(a);
+
+        String input = "";
+        int hour;
+        int min;
+        int sec;
+
+        while (scanner.hasNextLine())
+        {
+            input = scanner.next();
+            hour = scanner.nextInt();
+            min = scanner.nextInt();
+            sec = scanner.nextInt();
+
+           if(this.hasInstance(input))
+               this.exit(input, hour, min, sec);
+           else
+               this.entrance(input, hour, min, sec);
+        }
+
+        if(this.noOfCars.size() > 0)
+        {
+            System.out.println("\n	The following cars have not left the parking lot:");
+            this.displayAllCars();
+        }
+
+        System.out.println("\n	Total Revenue: $" + String.format("%.2f", this.getTotalRevenue()) + "\n");
+        scanner.close();
+
+    }
 
     public void entrance(String a, int b, int c, int d)
     {
@@ -42,21 +94,51 @@ public abstract class ParkingLot {
             noOfCars.push(carObject);
 
             String check = carObject.GetEntryTime();
-            System.out.println("Car " + carObject.getName() + " entered at: " + check);
+            System.out.println("	Car " + carObject.getName() + " entered at: " + check);
         }
         else
-            System.out.println("Sorry " + carObject.getName() + " no space available for parking");
+            System.out.println("	Sorry " + carObject.getName() + " no space available for parking");
     }
-
-    public void displayAllCars()
+    
+    public void exit(String a, int b, int c, int d)
     {
         int check = 0;
+        Car exitCar;
 
         while (check != noOfCars.size())
         {
-            Car display = noOfCars.get(check);
-            System.out.println(display.getName());
+            exitCar = noOfCars.get(check);
             check++;
+
+            if(exitCar.getName().equals(a)) {
+
+                exitCar.SetExitTime(b, c, d);
+                double priceOfTicket = pricing(exitCar);
+
+                totalRevenue += priceOfTicket;
+                
+                System.out.println("\n	" +exitCar.getName() + " is exiting the parking lot at " + exitCar.GetExitTime());
+                System.out.println("	The parking bill for " + exitCar.getName() + " is: $" + String.format("%.2f", priceOfTicket) + "\n");
+
+                noOfCars.remove(exitCar);
+
+                return;
+            }
         }
+
+        System.out.println(a + " car not Found");
+    }
+
+    double pricing(Car a)
+    {
+        int hoursSpent = a.getExithours() - a.getEntryhours();
+        int minsSpent = a.getExitmins() - a.getEntrymins();
+
+        double priceOfTicket = hoursSpent*pricePerHour;
+
+        if(minsSpent > 0)
+            priceOfTicket += pricePerHour;
+
+        return priceOfTicket;
     }
 }
